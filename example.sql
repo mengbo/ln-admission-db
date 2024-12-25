853c248d2635fa7494866ce7c3ad628c9a66f6fc
Meng Bo <mengbo@lnu.edu.cn>
2024-12-25 21:25:55 +0800

    增加一分一段表

--- example.sql
+++ example.sql
@@ -1,15 +1,15 @@
 SELECT
-s.min_score, p.plan_num, p.inst, p.major, p.inst_code, p.major_code
-FROM plan_2024 AS p JOIN score_phy2024 AS s
-ON p.inst_code = s.inst_code AND p.major_code = s.major_code
+s.min_score, r.rank, p.plan_num, s.inst, s.major, s.inst_code, s.major_code
+FROM plan_2024 AS p JOIN score_phy2024 AS s JOIN rank_phy2024 AS r
+ON p.inst_code = s.inst_code AND p.major_code = s.major_code AND s.min_score = r.score
 WHERE
-s.min_score BETWEEN 670 AND 720
-AND p.FIRST LIKE '%物%'
+p.FIRST LIKE '%物%'
 AND (
 	p.second LIKE '%化%'
 	OR p.second LIKE '%生%'
 	OR p.second LIKE '%不限%'
 )
+AND s.min_score BETWEEN 650 AND 720
 AND (
 	p.inst LIKE '%上海交通大学%'
 	OR p.inst LIKE '%浙江大学%'
