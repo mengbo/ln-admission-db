65ca0e9bc43999864901764875ee71b90e26cf8a
Meng Bo <mengbo@lnu.edu.cn>
2025-03-30 10:55:55 +0800

    修改数据库字段类型及数据库描述

--- createdb.sh
+++ createdb.sh
@@ -4,9 +4,29 @@ DB_PATH="./data.sqlite3"
 
 rm -rf $DB_PATH
 
-for CSV_FILE in *.csv; do
+for CSV_FILE in plan_*.csv; do
 	TABLE_NAME=$(basename -- "$CSV_FILE" .csv)
 	echo "Importing $CSV_FILE to $TABLE_NAME..."
+	sqlite3 $DB_PATH "CREATE TABLE $TABLE_NAME 
+		( batch TEXT, first TEXT, second TEXT, inst_code TEXT,
+		inst TEXT, major_code TEXT, major TEXT, plan_num INTEGER);"
+	sqlite3 $DB_PATH ".import --csv $CSV_FILE $TABLE_NAME"
+	echo "Import completed for $CSV_FILE."
+done
+
+for CSV_FILE in rank_*.csv; do
+	TABLE_NAME=$(basename -- "$CSV_FILE" .csv)
+	echo "Importing $CSV_FILE to $TABLE_NAME..."
+	sqlite3 $DB_PATH "CREATE TABLE $TABLE_NAME (score INTEGER, number INTEGER, rank INTEGER);"
+	sqlite3 $DB_PATH ".import --csv $CSV_FILE $TABLE_NAME"
+	echo "Import completed for $CSV_FILE."
+done
+
+for CSV_FILE in score_*.csv; do
+	TABLE_NAME=$(basename -- "$CSV_FILE" .csv)
+	echo "Importing $CSV_FILE to $TABLE_NAME..."
+	sqlite3 $DB_PATH "CREATE TABLE $TABLE_NAME 
+		(inst_code TEXT, inst TEXT, major_code TEXT, major TEXT, min_score INTEGER);"
 	sqlite3 $DB_PATH ".import --csv $CSV_FILE $TABLE_NAME"
 	echo "Import completed for $CSV_FILE."
 done
